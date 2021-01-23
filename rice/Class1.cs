537d8ada521df2d7db374ef06195b8779774feab
Peter <petahsu@gmail.com>
2021-01-24 01:05:43 +0800

    ,

--- rice/Class1.cs
+++ rice/Class1.cs
@@ -2,7 +2,15 @@
 
 namespace rice
 {
+    /// <summary>
+    /// 我的類別
+    /// </summary>
     public class Class1
     {
+        /// <summary>
+        /// 打招呼
+        /// </summary>
+        public void Greeting()
+        { }
     }
 }
